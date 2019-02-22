ee2caf849d6bdb5697a9b2515ffd9b1883e019c5
Daniel Cano <danicanoand@gmail.com>
2019-02-22 12:03:58 +0100

    kserver + ksshd

--- k18:kserver/install.sh
+++ k18:kserver/install.sh
@@ -14,4 +14,9 @@ kadmin.local -q "addprinc -pw kpau pau"
 kadmin.local -q "addprinc -pw kmarta marta/admin"
 kadmin.local -q "addprinc -pw kjulia julia"
 kadmin.local -q "addprinc -pw ksuperuser superuser"
+kadmin.local -q "addprinc -pw kuser01 user01"
+kadmin.local -q "addprinc -pw kuser03 user03"
+kadmin.local -q "addprinc -pw kuser02 user02"
+kadmin.local -q "addprinc -randkey host/sshd.edt.org"
 
+#kadmin.local -q "ktadd -k /etc/krb5.keytab host/sshd.edt.org"    exportar claus
--- k18:sshd/README.md
+++ k18:sshd/README.md
@@ -13,5 +13,7 @@ docker netweork create mynet
 docker run --rm --name kserver.edt.org -h kserver.edt.org --network mynet -d danicano/k18:kserver
 
 docker run --rm --name khost -h khost --network mynet -it danicano/k18:khost
+
+docker run --rm --name sshd.edt.org -h sshd.edt.org --network mynet -d danicano/k18:sshd
 ```
 
