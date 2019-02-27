b1740fa55ffa77a4bfa1b9d03495a229466f7da1
Daniel Cano <danicanoand@gmail.com>
2019-02-27 11:29:11 +0100

    khostp

--- k18_khostp/install.sh
+++ k18_khostp/install.sh
@@ -13,9 +13,5 @@ useradd -g users -G kusers user03
 echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
-/usr/bin/ssh-keygen -A
 cp /opt/docker/krb5.conf /etc/krb5.conf
-cp /opt/docker/sshd_config /etc/ssh/sshd_config
-kadmin -p pau -w kpau -q "ktadd -k /etc/krb5.keytab host/sshd.edt.org" #exportar claus
-
-#klist -k /etc/krb5.keytab     llistar les claus
+cp /opt/docker/system-auth /etc/pam.d/system-auth
